971780fd03b82ab9b9a71c622dd546bff0ce528f
danieleTabanella <daniele.tabanella@gmail.com>
2018-06-14 21:13:01 +0200

    test: one led

--- led-controller.js
+++ led-controller.js
@@ -5,35 +5,11 @@ module.exports = function (RED) {
     RED.nodes.createNode(this, config);
     var node = this;
     var lc = new LedControl();
-    lc.setBrightness(0,15);
+    // lc.setBrightness(0,15);
 
     node.on('input', function (msg) {
 
-
-      var d1 = {x : 0, y:0, xvel:1, yvel :1};
-      var d2 = {x : 0, y:0, xvel:1, yvel :1};
-
-      var  startTime = Date.now();
-      setInterval(loop, 10);
-
-      function loop() {
-        with(d1) {
-          lc.setLed(0,x,y,0);
-          lc.setLed(0,7-x,7-y,0);
-
-          x+=xvel;
-
-          if((x==8)||(x==-1)) {
-            x-=xvel;
-            xvel*=-1;
-            y+=yvel;
-            if((y==7)||(y==0)) yvel*=-1;
-          }
-
-          lc.setLed(0,x,y,1);
-          lc.setLed(0,7-x,7-y,1);
-        }
-      }
+      lc.setLed(0,1,1,0);
 
       lc.emitter.on('update', function ({
         dataPin,
