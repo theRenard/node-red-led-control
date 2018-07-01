d50a405de637ad08b5f86d52675f16eee454bf02
danieleTabanella <daniele.tabanella@gmail.com>
2018-07-01 18:50:48 +0200

    feat: add dynamic variables

--- led-controller.js
+++ led-controller.js
@@ -2,22 +2,26 @@
 var LedControl = require("rpi-led-control");
 var displayText = require("./display-text");
 
-var text = 'hello darling';
-var delay = 500;
-var display = new LedControl(12,11,6); // data pin, clock pin, cs pin
 
 module.exports = function (RED) {
   function LedController(config) {
     RED.nodes.createNode(this, config);
     var node = this;
-    display.setBrightness(0,15);
+
+    var brightness = config.brightness; // ex: 15
+    var delay = config.delay; // ex: 500
+    var dataPin = config.dataPin; // ex: 12
+    var clockPin = config.clockPin; // ex: 11
+    var csPin = config.csPin; // ex: 6
+
+    var display = new LedControl(dataPin, clockPin, csPin); // data pin, clock pin, cs pin
+    display.setBrightness(0,brightness);
 
     node.on('input', function (msg) {
 
       console.log(config);
       displayText(msg.payload, delay, display, function() {
-        console.log('callback');
-        // callback
+        node.send(msg);
       });
 
     });
